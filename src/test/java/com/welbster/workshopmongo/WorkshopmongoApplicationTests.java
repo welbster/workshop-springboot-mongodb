719f45ce08f76c949d92ccd33a3ee196abba79db
Welbster Uchoas <welbsteruchoas@gmail.com>
2024-03-30 12:25:18 -0300

    Primeiro commit

---
+++ src/test/java/com/welbster/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welbster.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
