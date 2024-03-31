35d18a173dbbcbc139e36353c8c6f47a36698a44
Welbster Uchoas <welbsteruchoas@gmail.com>
2024-03-31 12:53:24 -0300

    Obtendo um usuário por id

--- src/main/java/com/welbster/workshopmongo/services/UserService.java
+++ src/main/java/com/welbster/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.welbster.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.welbster.workshopmongo.domain.User;
 import com.welbster.workshopmongo.repository.UserRepository;
+import com.welbster.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,4 +18,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	}
+
