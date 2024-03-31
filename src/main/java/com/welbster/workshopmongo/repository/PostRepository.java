5afee186978e82f0d4c75eb51287194db8b0f280
Welbster Uchoas <welbsteruchoas@gmail.com>
2024-03-31 18:12:53 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/welbster/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.welbster.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.welbster.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+
+}
