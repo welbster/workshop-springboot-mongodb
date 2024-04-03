35b39a1349a898152e1137abe3849c3ce0a4d682
Welbster Uchoas <welbsteruchoas@gmail.com>
2024-04-03 19:54:36 -0300

    Obtendo um post por id

---
+++ src/main/java/com/welbster/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.welbster.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.welbster.workshopmongo.domain.Post;
+import com.welbster.workshopmongo.repository.PostRepository;
+import com.welbster.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
